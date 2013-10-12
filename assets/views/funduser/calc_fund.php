a2e06db37e39503bb1d183e7b027b359d52b1864
Repp Denis <Drepp@aplana.com>
2013-10-12 21:40:48 +0600

    losted files

---
+++ assets/views/funduser/calc_fund.php
@@ -0,0 +1,88 @@
+<h3>Расчет фонда стимулурующих выплат для преподавателей</h3>
+<form id="form" method="POST">
+    <fieldset>
+        <label>Факультет</label>
+        <select id="faculty" name="faculty">
+            <?php
+            foreach($faculties as $f) {
+                echo '<option value="'.$f->id.'">'.$f->name.'</option>';
+            }
+            ?>
+        </select>
+        <label>Этап</label>
+        <?php
+        foreach ($stages as $s) {
+            echo '<input type="radio" name="stage" value="'.$s->id.'" required />'.$s->name.'<br/>';
+        }
+        ?>
+        <br/>
+        <label>Год</label>
+        <select id="year" name="year" class="year">
+            <?php
+            $year = date("Y");
+            for ($i = 1970; $i< $year + 20; $i++) {
+                if ($i == $year) {
+                    echo '<option value="'.$i.'" selected>'.$i.'</option>';
+                } else {
+                    echo '<option value="'.$i.'">'.$i.'</option>';
+                }
+            }
+            ?>
+        </select>
+        <br/>
+        <br/>
+<!--        <label>Количество студентов университета (приведенный контингент)</label>-->
+<!--        <input type="number" name="nu" value="--><?php //getVal($nu_calc);?><!--" required/>-->
+<!--        <br/>-->
+<!--        <label>Количество штатных преподавателей в университете</label>-->
+<!--        <input type="number" name="npru" value="--><?php //getVal($npru_calc);?><!--" required/>-->
+
+        <button id="next_btn" type="button" class="btn">Рассчитать</button>
+        <button type="submit" class="hidden" id="submit_btn" />
+    </fieldset>
+</form>
+
+<script>
+    $(document).ready(function() {
+        $("#next_btn").click(function() {
+            // если форма не валидна - выходим
+            var form = $('#form')[0];
+            if (!form.checkValidity()) {
+                $('#form').find('#submit_btn').click();
+                return;
+            }
+
+            // ищем этап и в путь
+            var s = $('#form').serialize();
+            var vals = s.split("&");
+            var stage = 0;
+            for (var i=0; i < vals.length; i++) {
+                if (vals[i].startsWith("stage")) {
+                    stage = vals[i].substr(vals[i].lastIndexOf("=")+1);
+                }
+            }
+            $.ajax({
+                type: "GET",
+                url: "/ajax/check_funduser_calc_count",
+                data: {
+                    id: $('#faculty').val(),
+                    stage: stage,
+                    year:$('#year').val()
+                },
+                success: function (res) {
+                    if (res == 0) {
+                        console.log("вопрос");
+                        if (confirm("Расчет для факультета, года и этапа еще не существует расчетов баллов, расчет выплат не возможен. \n Перейти на страницу расчета баллов?")) {
+                            location.href="/awarduser/";
+                        }
+                    } else {
+                        $("#form").submit();
+                    }
+                },
+                error: function(res) {
+                    $.jGrowl("Произошла ошибка во время запроса к серверу");
+                }
+            });
+        });
+    });
+</script>
---
+++ classes/App/Controller/overall.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Controller;
+
+class Overall extends \App\Page {
+
+    public function action_index(){
+        if(!$this->logged_in('admin'))
+            return;
+        $this->view->message = $this->pixie->auth->user()->email;
+        $this->redirect("/overall/list/");
+    }
+
+    private function get_fsf($faculty,$stage,$year) {
+//        $fac = $this->pixie->orm->get('user')->with('faculty')->where('id',$user)->find();
+        $res = $this->pixie->orm->get('operation')->with('award')->where('a0.faculties_id',$faculty)->where('a0.stage_id',$stage)->where('a0.year',$year)->find();
+        if ($res->loaded()) {
+            return $res->money;
+        } else {
+           return null;
+        }
+    }
+
+    /**
+     * отображает скисок пользователей и денги выделенные им
+     */
+    public function action_list() {
+        if(!$this->logged_in())
+            return;
+
+        $dir = 'asc';
+        $d = $this->request->get('dir');
+        if ($d != null && $d == 'desc') {
+            $dir = 'desc';
+        }
+
+        $sort = $this->request->get('sort');
+
+        $year = $this->request->param('year');
+        $stage_id = $this->request->param('stage');
+        $awards = null;
+        switch ($sort) {
+            case 'fio':
+                $awards = $this->pixie->orm->get('operationuser')->with('awarduser','awarduser.user')->where('a0.year',$year)->where('a0.stage_id',$stage_id)->order_by('a1.fio',$dir)->find_all();
+                break;
+           default :
+               $awards = $this->pixie->orm->get('operationuser')->with('awarduser')->where('a0.year',$year)->where('a0.stage_id',$stage_id)->order_by('operation_user.money',$dir)->find_all();
+        }
+
+        $this->view->stage = $stage_id;
+        $this->view->awards = $awards;
+        $this->view->stages = $this->pixie->orm->get('stage')->find_all();
+        $this->view->year = $year;
+        $this->view->subview = '/funduser/list_fund';
+    }
+
+    /**
+     * отоброжает список всех сформированных расчетов
+     */
+    public function action_list_calc() {
+        if(!$this->logged_in())
+            return;
+
+        $dir = 'asc';
+        $year = $this->request->param('year');
+        if ($year == null) {
+            $year = date("Y");
+        }
+        $d = $this->request->get('dir');
+        if ($d != null && $d == 'desc') {
+            $dir = 'desc';
+        }
+
+        $sort = $this->request->get('sort');
+
+//        $year = $this->request->param('year');
+//        $stage_id = $this->request->param('stage');
+//        $awards = null;
+//        switch ($sort) {
+//            case 'faculty':
+//                $awards = $this->pixie->orm->get('awarduser')->with('faculty')->where('year',$year)->where('stage_id',$stage_id)->order_by('name',$dir)->find_all();
+//                break;
+//            default :
+//                $awards = $this->pixie->orm->get('awarduser')->with('faculty')->where('year',$year)->where('stage_id',$stage_id)->order_by('awards_users.money',$dir)->find_all();
+//        }
+        $user = $this->view->user = $this->pixie->auth->user();
+        $calcs = $this->pixie->orm->get('calcfunduser')->with('stage')->find_all();
+//        $this->view->stage = $stage_id;
+        $this->view->calcs = $calcs; // в таблицу
+//        $this->view->stages = $this->pixie->orm->get('stage')->find_all(); // в комбик
+//        $this->view->year = $year;
+        $this->view->subview = '/funduser/list_calc';
+    }
+}
