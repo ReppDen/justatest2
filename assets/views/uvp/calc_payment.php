5adbabe2e2864acbf4d5d6f93cc68a0823ff78d9
Repp Denis <reppden@gmail.com>
2013-10-15 22:48:01 +0600

    uvp crash fix

---
+++ assets/views/uvp/calc_payment.php
@@ -0,0 +1,33 @@
+<h3>Расчет выплат УВП ПГГПУ</h3>
+<form method="POST">
+    <fieldset>
+        <div class="alert alert-error">
+            Не забудьте проверить, что все расчеты баллов для <a href="/uvp/list_calc">УВП</a> заполнены!
+        </div>
+        <label>Фонд стимулирущих выплат университета в рублях</label>
+        <input id="fond" name="sum" type="number" required=""/>
+        <label>Этап</label>
+        <?php
+        foreach ($stages as $s) {
+            echo '<input type="radio" name="stage" value="'.$s->iduvp_stage.'" required />'.$s->name.'<br/>';
+        }
+        ?>
+        <br/>
+        <label>Год</label>
+        <select id="year" name="year" class="year">
+            <?php
+            $year = date("Y");
+            for ($i = 2012; $i< $year + 2; $i++) {
+                if ($i == $year) {
+                    echo '<option value="'.$i.'" selected>'.$i.'</option>';
+                } else {
+                    echo '<option value="'.$i.'">'.$i.'</option>';
+                }
+            }
+            ?>
+        </select>
+        <br/>
+        <br/>
+        <button type="submit" class="btn">Рассчитать</button>
+    </fieldset>
+</form>
