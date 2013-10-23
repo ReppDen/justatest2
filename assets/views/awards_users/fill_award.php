5606d6de7c1a603b5a362123f91acfc67b8b306d
Repp Den <ReppDen@gmail.com>
2013-10-24 00:21:54 +0600

    исправлениы границы изменения полей ввода

--- assets/views/awards_users/fill_award.php
+++ assets/views/awards_users/fill_award.php
@@ -7,39 +7,39 @@ function first_stage()
     <label>Индекс научной эффективности преподавателя</label>
     число аспирантов, защитившихся в отчетный период
     <input type="hidden" name="o7_2_name" value="число аспирантов, защитившихся в отчетный период"/>
-    <br/><input type="number" name="o7_2" value="0" required/>
+    <br/><input type="number" name="o7_2" value="0" min="0" max="10000" required/>
     <br/><br/>
     число докторантов, защитившихся в отчетный период
     <input type="hidden" name="o7_1_name" value="число докторантов, защитившихся в отчетный период"/>
-    <br/><input type="number" name="o7_1" value="0" required/>
+    <br/><input type="number" name="o7_1" value="0" min="0" max="10000" required/>
     <br/><br/>
     руководство грантами РФФИ, РГНФ, федеральных  целевых Программ, Программы стратегического развития вуза
     <input type="hidden" name="o7_3_name" value="руководство грантами РФФИ, РГНФ, федеральных  целевых Программ, Программы стратегического развития вуза"/>
-    <br/><input type="number" name="o7_3" value="0" required/>
+    <br/><input type="number" name="o7_3" value="0" min="0" max="10000" required/>
     <br/><br/>
     организация конференций любого уровня
     <input type="hidden" name="o7_4_name" value="организация конференций любого уровня"/>
-    <br/><input type="number" name="o7_4" value="0" required/>
+    <br/><input type="number" name="o7_4" value="0" min="0" max="10000" required/>
     <br/><br/>
     выступления сотрудников с докладами на международных и всероссийских конференциях
     <input type="hidden" name="o7_5_name" value="выступления сотрудников с докладами на международных и всероссийских конференциях"/>
-    <br/><input type="number" name="o7_5" value="0" required/>
+    <br/><input type="number" name="o7_5" value="0" min="0" max="10000" required/>
     <br/><br/>
     количество монографий с  ISBN, или разделов в коллективных монографиях,  учебников с грифом МОН РФ, Рособразования, УМО
     <input type="hidden" name="o7_6_name" value="количество монографий с  ISBN, или разделов в коллективных монографиях,  учебников с грифом МОН РФ, Рособразования, УМО"/>
-    <br/><input type="number" name="o7_6" value="0" required/>
+    <br/><input type="number" name="o7_6" value="0" min="0" max="10000" required/>
     <br/><br/>
     количество статей в рецензируемых (ВАК) изданиях
     <input type="hidden" name="o7_7_name" value="количество статей в рецензируемых (ВАК) изданиях"/>
-    <br/><input type="number" name="o7_7" value="0" required/>
+    <br/><input type="number" name="o7_7" value="0" min="0" max="10000" required/>
     <br/><br/>
     в зарубежных индексируемых изданиях
     <input type="hidden" name="o7_8_name" value="в зарубежных индексируемых изданиях"/>
-    <br/><input type="number" name="o7_8" value="0" required/>
+    <br/><input type="number" name="o7_8" value="0" min="0" max="10000" required/>
     <br/><br/>
     количество статей в других изданиях
     <input type="hidden" name="o7_9_name" value="количество статей в других изданиях"/>
-    <br/><input type="number" name="o7_9" value="0" required/>
+    <br/><input type="number" name="o7_9" value="0" min="0" max="10000" required/>
 </fieldset>
 ';
 }
--- assets/views/ouk/fill_ouk.php
+++ assets/views/ouk/fill_ouk.php
@@ -10,47 +10,47 @@ function first_stage()
     <br/><br/>
     число аспирантов, защитившихся в отчетный период
     <input type="hidden" name="o7_2_name" value="число аспирантов, защитившихся в отчетный период"/>
-    <br/><input type="number" name="o7_2" value="0" required/>
+    <br/><input type="number" name="o7_2" value="0" min="0" max="10000" required/>
     <br/><br/>
     число докторантов, защитившихся в отчетный период
     <input type="hidden" name="o7_3_name" value="число докторантов, защитившихся в отчетный период"/>
-    <br/><input type="number" name="o7_3" value="0" required/>
+    <br/><input type="number" name="o7_3" value="0" min="0" max="10000" required/>
     <br/><br/>
     наличие грантов РФФИ, РГНФ, федеральных целевых программ, ПСР вуза
     <input type="hidden" name="o7_3a_name" value="наличие грантов РФФИ, РГНФ, федеральных целевых программ, ПСР вуза"/>
-    <br/><input type="number" name="o7_3a" value="0" required/>
+    <br/><input type="number" name="o7_3a" value="0" min="0" max="10000" required/>
     <br/><br/>
     организация конференций любого уровня
     <input type="hidden" name="o7_4_name" value="организация конференций любого уровня"/>
-    <br/><input type="number" name="o7_4" value="0" required/>
+    <br/><input type="number" name="o7_4" value="0" min="0" max="10000" required/>
     <br/><br/>
     выступления сотрудников с докладами на международных и всероссийских конференциях
     <input type="hidden" name="o7_5_name" value="выступления сотрудников с докладами на международных и всероссийских конференциях"/>
-    <br/><input type="number" name="o7_5" value="0" required/>
+    <br/><input type="number" name="o7_5" value="0" min="0" max="10000" required/>
     <br/><br/>
     количество монографий с  ISBN, или разделов в коллективных монографиях,  учебников с грифом МОН РФ, Рособразования, УМО
     <input type="hidden" name="o7_6_name" value="количество монографий с  ISBN, или разделов в коллективных монографиях,  учебников с грифом МОН РФ, Рособразования, УМО в России"/>
-    <br/>Изданий в РФ<input type="number" name="o7_6" value="0" required/>
+    <br/>Изданий в РФ<input type="number" name="o7_6" value="0" min="0" max="10000" required/>
     <input type="hidden" name="o7_6a_name" value="количество монографий с  ISBN, или разделов в коллективных монографиях,  учебников с грифом МОН РФ, Рособразования, УМО за рубежом"/>
-    <br/>Изданий за рубежом<input type="number" name="o7_6a" value="0" required/>
+    <br/>Изданий за рубежом<input type="number" name="o7_6a" value="0" min="0" max="10000" required/>
     <br/><br/>
 
     количество учебников с грифом МОН РФ, Рособразования,УМО
     <input type="hidden" name="o7_6b_name" value="количество учебников с грифом МОН РФ, Рособразования,УМО"/>
-    <br/><input type="number" name="o7_6b" value="0" required/>
+    <br/><input type="number" name="o7_6b" value="0" min="0" max="10000" required/>
     <br/><br/>
 
     количество статей в рецензируемых (ВАК) изданиях
     <input type="hidden" name="o7_7_name" value="количество статей в рецензируемых (ВАК) изданиях"/>
-    <br/><input type="number" name="o7_7" value="0" required/>
+    <br/><input type="number" name="o7_7" value="0" min="0" max="10000" required/>
     <br/><br/>
     в зарубежных индексируемых изданиях
     <input type="hidden" name="o7_8_name" value="в зарубежных индексируемых изданиях"/>
-    <br/><input type="number" name="o7_8" value="0" required/>
+    <br/><input type="number" name="o7_8" value="0" min="0" max="10000" required/>
     <br/><br/>
     количество статей в других изданиях
     <input type="hidden" name="o7_9_name" value="количество статей в других изданиях"/>
-    <br/><input type="number" name="o7_9" value="0" required/>
+    <br/><input type="number" name="o7_9" value="0" min="0" max="10000" required/>
 </fieldset>
 ';
 }
@@ -61,7 +61,7 @@ function second_stage()
     <label>Проектирование и разработка образовательных программ (аспирантура)</label>
     Разработка и реализация программ аспирантуры. За каждую реализуемую ООП
     <input type="hidden" name="o2_1_name" value="Разработка и реализация программ аспирантуры. За каждую реализуемую ООП"/>
-    <br/><input type="number" name="o2_1" value="0" required/>
+    <br/><input type="number" name="o2_1" value="0" min="0" max="10000" required/>
     <br/><br/>
 
 
@@ -136,3 +136,6 @@ function third_stage()
 
     </fieldset>';
 }
+
+
+
